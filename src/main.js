b27d1207d07e15bf46608abdf94557084d4f0500
edenave <76529301+edenave@users.noreply.github.com>
2021-01-28 15:18:02 +0200

    append priority selcted with inputtext

--- src/main.js
+++ src/main.js
@@ -2,17 +2,23 @@ const addButton = document.getElementById("add-button");
 
 
 
-const prioritySelector = document.getElementById("priority-selector");
+
 
 const tasksList = document.getElementById("taskslist");
 
 addButton.addEventListener("click", submitToList)
 
 function submitToList() {
+
         const listItem = document.createElement("Li");
         const text = document.getElementById("text-input").value; 
+        const selectedItem = document.getElementById("priority-selector").value;
+        
         const listValue=document.createTextNode(text);
-        listItem.appendChild(listValue);
+        listItem.append(selectedItem,". " ,listValue);
         document.getElementById("taskslist").appendChild(listItem);
     }
+
+
+   
 
