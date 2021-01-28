a3bb427d32e0a9f35dbcdbf6e2329f33e76e1d22
edenave <76529301+edenave@users.noreply.github.com>
2021-01-28 14:39:21 +0200

    add eventlistener for add button to add text

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-const addButton = document.getElementById("add-button");   
-const textInput = document.getElementById("text-input");
+const addButton = document.getElementById("add-button");
+
+
+
 const prioritySelector = document.getElementById("priority-selector");
-const tasksList = document.getElementById("tasksList");
+
+const tasksList = document.getElementById("taskslist");
 
 addButton.addEventListener("click", submitToList)
 
 function submitToList() {
-    const listItem = document.createElement("li");
-        const inputText = document.createTextNode(textInput);
-        listItem.appendChild(inputText);
-        if(inputText===""){
-            alert("please write something");
-        }else{
-            tasksList.appendChild(listItem);
-        }
+        const listItem = document.createElement("Li");
+        const text = document.getElementById("text-input").value; 
+        const listValue=document.createTextNode(text);
+        listItem.appendChild(listValue);
+        document.getElementById("taskslist").appendChild(listItem);
+    }
 
-}
