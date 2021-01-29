d4dba0b9cb143e1adf579d54780ac6fcdf557d8f
edenave <76529301+edenave@users.noreply.github.com>
2021-01-29 10:44:21 +0200

    added divs
    container div time created div priority div, 4 tests passed

--- src/main.js
+++ src/main.js
@@ -11,12 +11,48 @@ addButton.addEventListener("click", submitToList)
 function submitToList() {
 
         const listItem = document.createElement("Li");
-        const text = document.getElementById("text-input").value; 
+        const text = document.getElementById("text-input").value;
         const selectedItem = document.getElementById("priority-selector").value;
-        
+        const selectedOption = document.getElementById("priority-selector");
         const listValue=document.createTextNode(text);
-        listItem.append(selectedItem,". " ,listValue);
+
+
+        const containerDiv = document.createElement("div");
+
+        containerDiv.className = "todo-container";
+
+        const timeCreatedDiv = document.createElement("div");
+
+        timeCreatedDiv.className = "todo-created-at";
+
+        const priorityDiv = document.createElement("div");
+        
+        priorityDiv.className = "todo-priority";
+        const textDiv = document.createElement("div");
+        textDiv.className = "todo-text";
+        if(text === ''){
+            alert('no task entered')
+        } else {
+
+            textDiv.append(listValue);
+            priorityDiv.append(selectedItem);
+            timeCreatedDiv.append(new Date().toISOString().slice(0, 19).replace('T', ' '));
+
+            containerDiv.append(priorityDiv, timeCreatedDiv, textDiv);
+
+        listItem.append(containerDiv);
+
+
         document.getElementById("taskslist").appendChild(listItem);
+        document.getElementById("text-input").value = '';
+        
+
+
+
+        selectedOption.remove(selectedOption.selectedIndex);
+        
+        }
+
     }
 
 
