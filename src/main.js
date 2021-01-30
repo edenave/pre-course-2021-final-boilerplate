28b71c64dc472180cb64a54e64dbd4661d360186
edenave <76529301+edenave@users.noreply.github.com>
2021-01-30 19:31:56 +0200

    css body abjustments

--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ const addButton = document.getElementById("add-button");
 
 const savedTasks = document.getElementById("taskslist");
 
-function saveData(){
-  localStorage.setItem("taskslist", savedTasks.innerHTML);
+function saveData() {
+    localStorage.setItem("taskslist", savedTasks.innerHTML);
 }
 
-let localsaved =localStorage.getItem("taskslist");
-  savedTasks.innerHTML = localsaved;
+let localsaved = localStorage.getItem("taskslist");
+savedTasks.innerHTML = localsaved;
 
 
 const tasksList = document.getElementById("taskslist");
@@ -18,119 +18,119 @@ addButton.addEventListener("click", submitToList)
 
 function submitToList() {
 
-        const listItem = document.createElement("Li");
-        const text = document.getElementById("text-input").value;
-        const selectedItem = document.getElementById("priority-selector").value;
-        const selectedOption = document.getElementById("priority-selector");
-        const listValue=document.createTextNode(text);
+    const listItem = document.createElement("Li");
+    const text = document.getElementById("text-input").value;
+    const selectedItem = document.getElementById("priority-selector").value;
+    const listValue = document.createTextNode(text);
 
 
-        const containerDiv = document.createElement("div");
+    const containerDiv = document.createElement("div");
 
-        containerDiv.className = "todo-container";
+    containerDiv.className = "todo-container";
 
-        const timeCreatedDiv = document.createElement("div");
+    const timeCreatedDiv = document.createElement("div");
 
-        timeCreatedDiv.className = "todo-created-at";
+    timeCreatedDiv.className = "todo-created-at";
 
-        const priorityDiv = document.createElement("div");
+    const priorityDiv = document.createElement("div");
 
-        priorityDiv.className = "todo-priority";
-        const textDiv = document.createElement("div");
-        textDiv.className = "todo-text";
-        if(text === ''){
-            alert('no task entered')
-        } else {
+    priorityDiv.className = "todo-priority";
+    const textDiv = document.createElement("div");
+    textDiv.className = "todo-text";
+    if (text === '') {
+        alert('no task entered')
+    } else {
 
-            textDiv.append(listValue);
-            priorityDiv.append(selectedItem);
-            timeCreatedDiv.append(new Date().toISOString().slice(0, 19).replace('T', ' '));
+        textDiv.append(listValue);
+        priorityDiv.append(selectedItem);
+        timeCreatedDiv.append(new Date().toISOString().slice(0, 19).replace('T', ' '));
 
-            containerDiv.append(priorityDiv, timeCreatedDiv, textDiv);
+        containerDiv.append(priorityDiv, timeCreatedDiv, textDiv);
 
         listItem.append(containerDiv);
 
-            
+
         document.getElementById("taskslist").appendChild(listItem);
         document.getElementById("text-input").value = '';
         saveData()
-            count();
+        count();
 
 
 
-            
 
-            
 
-        selectedOption.remove(selectedOption.selectedIndex);
-        
-        }
+
+
+ 
 
     }
 
-
-
-
-    const counter = document.getElementById("counter")
-    let savedCount = localStorage.getItem("counter");
-    function count(){
-
-        let ul = document.getElementById('taskslist');
-        let i=0, count =0;
-        while(ul.getElementsByTagName('li')[i++]) count++;
-        counter.innerHTML = count;
-        localStorage.setItem("counter", counter.innerText); 
+}
 
 
 
 
-        }
-        counter.innerHTML = savedCount;
+const counter = document.getElementById("counter")
+let savedCount = localStorage.getItem("counter");
+function count() {
+
+    let ul = document.getElementById('taskslist');
+    let i = 0, count = 0;
+    while (ul.getElementsByTagName('li')[i++]) count++;
+    counter.innerHTML = count;
+    localStorage.setItem("counter", counter.innerText);
+
+
+
+
+}
+counter.innerHTML = savedCount;
 
 const sortButton = document.getElementById("sort-button");
 
-sortButton.addEventListener("click", sortTasks)
-    function sortTasks(){
-            list = document.getElementById("taskslist");
-            priorityDiv = document.getElementsByClassName("todo-priority");
+sortButton.addEventListener("click", sortTasks);
 
-                let i
-                let switching;
-                let shouldSwitch;
-                switching = true;
-               
+function sortTasks() {
+    list = document.getElementById("taskslist");
+    priorityDiv = document.getElementsByClassName("todo-priority");
 
-                while (switching) {
-
-                  switching = false;
-                  b = list.getElementsByClassName("todo-priority");
-                  li = document.getElementsByTagName("li");
-
-                  
-                  for (i = 0; i < (b.length - 1); i++) {
+    let i
+    let switching;
+    let shouldSwitch;
+    switching = true;
 
 
-                    shouldSwitch = false;
+    while (switching) {
 
-                    
-                    if (Number(b[i].innerHTML) < Number(b[i + 1].innerHTML)) {
+        switching = false;
+        b = list.getElementsByClassName("todo-priority");
+        li = document.getElementsByTagName("li");
 
-                        
-                      shouldSwitch = true;
-                      break;
-                    }
-                  }
-                  if (shouldSwitch) {
 
-                    
-                    li[i].parentNode.insertBefore(li[i + 1], li[i]);
-                    switching = true;
-                    saveData()
+        for (i = 0; i < (b.length - 1); i++) {
 
-                  }
-                }
-              }
- 
+
+            shouldSwitch = false;
+
+
+            if (Number(b[i].innerHTML) < Number(b[i + 1].innerHTML)) {
+
+
+                shouldSwitch = true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+
+
+            li[i].parentNode.insertBefore(li[i + 1], li[i]);
+            switching = true;
+            saveData()
+
+        }
+    }
+}
+
 
 
 
