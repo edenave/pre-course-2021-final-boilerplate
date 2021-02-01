69898d197d75ef0515c34d9654fec9b770dc58d6
edenave <76529301+edenave@users.noreply.github.com>
2021-02-01 18:41:25 +0200

    adding darkmode

--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
 const addButton = document.getElementById("add-button");
 
-let remaining = 0;   
+let remaining = 0;
 
 const savedTasks = document.getElementById("taskslist");
 
 
-function saveData(){
+function saveData() {
   localStorage.setItem("taskslist", savedTasks.innerHTML);
 }
 
-let localsaved =localStorage.getItem("taskslist");
-  savedTasks.innerHTML = localsaved;
+let localsaved = localStorage.getItem("taskslist");
+savedTasks.innerHTML = localsaved;
 
 
 const tasksList = document.getElementById("taskslist");
@@ -19,154 +19,154 @@ addButton.addEventListener("click", submitToList)
 
 function submitToList() {
 
-        const listItem = document.createElement("li");
-        listItem.className = "li";
-        const text = document.getElementById("text-input").value;
-        const selectedItem = document.getElementById("priority-selector").value;
-        const selectedOption = document.getElementById("priority-selector");
-        const listValue=document.createTextNode(text);
+  const listItem = document.createElement("li");
+  listItem.className = "li";
+  const text = document.getElementById("text-input").value;
+  const selectedItem = document.getElementById("priority-selector").value;
+  const selectedOption = document.getElementById("priority-selector");
+  const listValue = document.createTextNode(text);
 
 
-        const containerDiv = document.createElement("div");
+  const containerDiv = document.createElement("div");
 
-        containerDiv.className = "todo-container";
+  containerDiv.className = "todo-container";
 
-        const timeCreatedDiv = document.createElement("div");
+  const timeCreatedDiv = document.createElement("div");
 
-        timeCreatedDiv.className = "todo-created-at";
+  timeCreatedDiv.className = "todo-created-at";
 
-        const priorityDiv = document.createElement("div");
+  const priorityDiv = document.createElement("div");
 
-        priorityDiv.className = "todo-priority";
-        const textDiv = document.createElement("div");
-        textDiv.className = "todo-text";
+  priorityDiv.className = "todo-priority";
+  const textDiv = document.createElement("div");
+  textDiv.className = "todo-text";
 
-        const deleteButton = document.createElement("button");
-        deleteButton.innerText = 'x';
-        deleteButton.className = "delete-button"
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = 'x';
+  deleteButton.className = "delete-button"
 
-        if(text === ''){
-            alert('no task entered')
-        } else {
+  if (text === '') {
+    alert('no task entered')
+  } else {
 
-            textDiv.append(listValue);
-            priorityDiv.append(selectedItem);
-            timeCreatedDiv.append(new Date().toISOString().slice(0, 19).replace('T', ' '));
+    textDiv.append(listValue);
+    priorityDiv.append(selectedItem);
+    timeCreatedDiv.append(new Date().toISOString().slice(0, 19).replace('T', ' '));
 
-            containerDiv.append(deleteButton, priorityDiv, timeCreatedDiv, textDiv);
-
-
-
-        listItem.append(containerDiv);
-
-            
-        document.getElementById("taskslist").appendChild(listItem);
-        document.getElementById("text-input").value = '';
-        saveData()
-            count();
-
-            remaining++;
-            document.getElementById('remaining').innerHTML =  remaining;
-            localStorage.setItem("remaining", document.getElementById('remaining').innerHTML); 
-
-            
-
-            
+    containerDiv.append(deleteButton, priorityDiv, timeCreatedDiv, textDiv);
 
 
-        
-        }
 
-    }
+    listItem.append(containerDiv);
+
+
+    document.getElementById("taskslist").appendChild(listItem);
+    document.getElementById("text-input").value = '';
+    saveData()
+    count();
+
+    remaining++;
+    document.getElementById('remaining').innerHTML = remaining;
+    localStorage.setItem("remaining", document.getElementById('remaining').innerHTML);
 
 
 
 
-    const counter = document.getElementById("counter")
-    let savedCount = localStorage.getItem("counter");
-    function count(){
 
-        let ul = document.getElementById('taskslist');
-        let i=0, count =0;
-        while(ul.getElementsByTagName('li')[i++]) count++;
-        counter.innerHTML = count;
 
-        localStorage.setItem("counter", counter.innerHTML); 
+
+  }
+
+}
 
 
 
 
-        }
-        counter.innerHTML = savedCount;
+const counter = document.getElementById("counter")
+let savedCount = localStorage.getItem("counter");
+function count() {
+
+  let ul = document.getElementById('taskslist');
+  let i = 0, count = 0;
+  while (ul.getElementsByTagName('li')[i++]) count++;
+  counter.innerHTML = count;
+
+  localStorage.setItem("counter", counter.innerHTML);
+
+
+
+
+}
+counter.innerHTML = savedCount;
 
 const sortButton = document.getElementById("sort-button");
 
 sortButton.addEventListener("click", sortTasks)
-    function sortTasks(){
-            list = document.getElementById("taskslist");
-            priorityDiv = document.getElementsByClassName("todo-priority");
+function sortTasks() {
+  list = document.getElementById("taskslist");
+  priorityDiv = document.getElementsByClassName("todo-priority");
 
-                let i
-                let switching;
-                let shouldSwitch;
-                switching = true;
-               
-
-                while (switching) {
-
-                  switching = false;
-                  b = list.getElementsByClassName("todo-priority");
-                  li = document.getElementsByTagName("li");
-
-                  
-                  for (i = 0; i < (b.length - 1); i++) {
+  let i
+  let switching;
+  let shouldSwitch;
+  switching = true;
 
 
-                    shouldSwitch = false;
+  while (switching) {
 
-                    
-                    if (Number(b[i].innerHTML) < Number(b[i + 1].innerHTML)) {
+    switching = false;
+    b = list.getElementsByClassName("todo-priority");
+    li = document.getElementsByTagName("li");
 
-                        
-                      shouldSwitch = true;
-                      break;
-                    }
-                  }
-                  if (shouldSwitch) {
 
-                    
-                    li[i].parentNode.insertBefore(li[i + 1], li[i]);
-                    switching = true;
-                    saveData()
+    for (i = 0; i < (b.length - 1); i++) {
 
-                  }
-                }
-              }
+
+      shouldSwitch = false;
+
+
+      if (Number(b[i].innerHTML) < Number(b[i + 1].innerHTML)) {
+
+
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+
+
+      li[i].parentNode.insertBefore(li[i + 1], li[i]);
+      switching = true;
+      saveData()
+
+    }
+  }
+}
 
 
 
 let list1 = document.querySelector('ul');
-list1.addEventListener('click', function(ev) {
+list1.addEventListener('click', function (ev) {
 
-    ev.target.classList.toggle('done');
-    ev.target.parentNode.classList.toggle('done')
-    saveData()
+  ev.target.classList.toggle('done');
+  ev.target.parentNode.classList.toggle('done')
+  saveData()
 
-  }, false);
+}, false);
 
 
-  list1.addEventListener('click', function(ev) {
-    if (ev.target.className === 'delete-button') {
-      ev.target.classList.toggle('delete');
-      ev.target.parentNode.classList.toggle('delete')
-        if(remaining > 0){
+list1.addEventListener('click', function (ev) {
+  if (ev.target.className === 'delete-button') {
+    ev.target.classList.toggle('delete');
+    ev.target.parentNode.classList.toggle('delete')
+    if (remaining > 0) {
       remaining--;
     }
-      document.getElementById('remaining').innerHTML =  remaining;
-      localStorage.setItem("remaining", document.getElementById('remaining').innerHTML); 
-saveData()
-    }
-  }, false);
+    document.getElementById('remaining').innerHTML = remaining;
+    localStorage.setItem("remaining", document.getElementById('remaining').innerHTML);
+    saveData()
+  }
+}, false);
 
 
 let savedRemaining = localStorage.getItem("remaining");
@@ -178,12 +178,36 @@ const resetButton = document.getElementById('reset');
 
 resetButton.addEventListener('click', removeLocalStorage)
 
-function removeLocalStorage(){
-    localStorage.removeItem("taskslist");
-    localStorage.removeItem("remaining");
-    localStorage.removeItem("counter");
-    counter.innerText = '';
+function removeLocalStorage() {
+  localStorage.removeItem("taskslist");
+  localStorage.removeItem("remaining");
+  localStorage.removeItem("counter");
+  counter.innerText = '';
 
-    document.getElementById('remaining').innerHTML = '';
-    savedTasks.innerHTML = '';
+  document.getElementById('remaining').innerHTML = '';
+  savedTasks.innerHTML = '';
+}
+
+
+
+const darkModeButton = document.getElementById("dark-mode");
+
+darkModeButton.addEventListener('click', getSpooky)
+
+function getSpooky(){
+  let body = document.body;
+  let viewSection = document.getElementById("view-section");
+  let controlSection = document.getElementById("control-section");
+  let everythingSection = document.getElementById("everything");
+  let headers = document.querySelector('.counter-head');
+  body.classList.toggle('darkMode');
+  viewSection.classList.toggle('everything-dark');
+  controlSection.classList.toggle('everything-dark');
+  everythingSection.classList.toggle('everything-dark');
+  headers.classList.toggle('darkHeader')
+let liDark = document.getElementsByClassName('li')
+let ul = document.getElementById('taskslist')
+  for(let i = 0; i < liDark.length; i++){
+    liDark[i].classList.toggle("liDark");
+  }
 }
--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,9 @@
-const API_KEY = '$2b$10$9WjiiP.4urgSZ6aU34lFAeqUhcOwQ6o1UdiaIjhNhHaLC4ccWXnnW'; // Assign this variable to your JSONBIN.io API key if you choose to use it.
+const API_KEY =; // Assign this variable to your JSONBIN.io API key if you choose to use it.
 const DB_NAME = "my-todo";
 
 // Gets data from persistent storage by the given key and returns it
 async function getPersistent(key) {
-  const res = await fetch('https://api.jsonbin.io/b/6017ee21dde2a87f921bb52c');
-  const text = await res.text();
-  console.log(text);
+
 
   
 
@@ -18,3 +16,4 @@ async function getPersistent(key) {
 async function setPersistent(key, data) {
   return true;
 }
+
